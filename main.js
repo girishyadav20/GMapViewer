9c1f1e628ed4228e859d383f4e4b9ad8d3b28ff9
girishyadav20 <girishyadav20@gmail.com>
2015-07-22 10:30:46 -0700

    edits in attribution

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var map = L.map("map").setView([34.05, -117.18], 12);
 	
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: '&cpoy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
